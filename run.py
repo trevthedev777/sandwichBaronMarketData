83fd8faadfdfd28cf7a914d41662b41fd341da8c
trevthedev777 <bloodsportrecords@gmail.com>
2022-03-25 15:43:54 +0000

    Add validation to check for exactly 6 values in provided data

--- run.py
+++ run.py
@@ -22,7 +22,24 @@ def get_sales_data():
     print('Data should be six numbers, seperated by commas(CSV).')
     print('Example: 10, 20, 30, 40, 50, 60\n')
 
-    data_str = input('Enter your data here: ')
-    print(f'The data provided is {data_str}')
+    data_str = input('Enter your data here: ') #User inputs data here
+
+    sales_data = data_str.split(',') #will remove commas from the string, the split() method returns the broken up values as a list
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values
+    """
+    try:
+       if len(values) != 6:
+           raise ValueError(
+               f'Exactly 6 values required, you provided {len(values)}'
+           )
+    except ValueError as e:
+        print(f'Invalid data: {e}, please try again.\n')
 
 get_sales_data()
